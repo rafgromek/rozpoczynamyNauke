fedeb6db64784eada01f187df30cdd8ebff6d7ef
Rafal Gromek <rafgromek@gmail.com>
2019-01-14 07:04:36 +0100

    Initial commit

---
+++ src/RozpocynamyNauke.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Rafal Gromek ON 14.01.2019.
+ */
+public class RozpocynamyNauke {
+
+    public static void main (String[] args) {
+
+        System.out.println("Rozpoczynamy nauke w Javie");
+        System.out.println("Rafal");
+        System.out.println(30);
+        System.out.println("R");
+    }
+}
